bf70e1bb65823f2f383d11ec9aa9c9325bb3c813
Mithilesh Kumar <dev@gub-bins.in>
2026-01-14 12:07:47 +0530

    Changed

--- system_spike_logger.sh
+++ system_spike_logger.sh
@@ -30,7 +30,7 @@ else
   done
 fi
 
-if [ "${CPU_INT:-99}" -ge $CPU_THRESHOLD ] || [ "${MEM_INT:-99}" -ge $MEM_THRESHOLD ]; then
+if [ "${CPU_INT:-80}" -ge $CPU_THRESHOLD ] || [ "${MEM_INT:-80}" -ge $MEM_THRESHOLD ]; then
   if [ ! -f "$CSV_LOG" ]; then
     echo "timestamp,hostname,pid,process,cpu_percent,mem_percent" >> "$CSV_LOG"
   fi
